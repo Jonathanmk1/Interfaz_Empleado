a1969d3d3e00d527117e6d9ed5c9ba0c71bfee8d
Jonathanmk1 <114133233+Jonathanmk1@users.noreply.github.com>
2022-11-23 23:12:14 -0600

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,12 @@
+
+package d11172022;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        FrmPrincipal v;
+        v = new FrmPrincipal();
+        v.setVisible(true);
+    }
+    
+}
